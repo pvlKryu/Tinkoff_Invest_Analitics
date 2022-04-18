3a6a9fd630978e238b1ce8cc8f5fb04b8870c401
Pavel Kriuchkov <57821178+pvlKryu@users.noreply.github.com>
2022-04-18 11:06:49 +1000

    Formal output
    The data is displayed not as a single custom type, but as lines by categories of securities

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from tinkoff.invest import Client
 import datetime
+import pandas as pd
 
 """
 DOCs:
@@ -39,14 +40,20 @@ def operations(c, id):  # Display all operations on the brokerage account in the
 
 def value(c, id):  # Display portfolio contents and value
     # Conversion of custom data type to string
-    t_string = str(c.operations.get_portfolio(account_id=id))
-    t_string.replace("),", "\n")
+    #t_string = str(c.operations.get_portfolio(account_id=id))
+    #t_string.replace("),", "\n")
     #t_string.split("), ")
     # print((c.operations.get_portfolio(account_id=id)))
-    for temp in t_string:
-        #t_string.replace("PortfolioResponse", "\n")
-        print(temp)
-    print(t_string)
+    # for temp in t_string:
+    #     t_string.replace("PortfolioResponse", "\n")
+    #     print(temp)
+    # print(t_string)
+    #r : p_response = c.operations.get_portfolio(account_id=id)
+    keys = ['total_amount_shares', 'total_amount_bonds',
+            'total_amount_etf', 'total_amount_currencies', 'total_amount_futures']
+    data = str({k: getattr(c.operations.get_portfolio(account_id=id), k)
+               for k in keys})  # formal output with keys
+    print(data)
 
 
 def cost_money(m):  # Function to convert custom currency format to Float
